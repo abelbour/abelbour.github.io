34e3f7b7268b8761b869a8092f8d9cd931af0e5f
abelbour <abelbour2k@yahoo.com>
2016-08-01 20:49:26 -0300

    first

---
+++ script.js
@@ -0,0 +1,45 @@
+var app=angular.module('single-page-app',['ngRoute']);
+
+
+app.config(function($routeProvider){
+
+
+      $routeProvider
+          .when('/',{
+                templateUrl: 'inicio.html'
+          })
+          .when('/predicacion',{
+                templateUrl: 'predicacion/index.html'
+          });
+          .when('/salidas',{
+                templateUrl: 'predicacion/salidas.html'
+          });
+          .when('/territorios',{
+                templateUrl: 'predicacion/territorios.html'
+          });         
+          .when('/informe',{
+                templateUrl: 'predicacion/informe.html'
+          });    
+          .when('/reuniones',{
+                templateUrl: 'reuniones/index.html'
+          });
+          .when('/vivo',{
+                templateUrl: 'reuniones/vivo.html'
+          }); 
+          .when('/finde',{
+                templateUrl: 'reuniones/finde.html'
+          });           
+          .when('/semana',{
+                templateUrl: 'reuniones/semana.html'
+          });
+          .when('/programa',{
+                templateUrl: 'reuniones/programa.html'
+          });           
+});
+
+
+app.controller('cfgController',function($scope){
+
+      $scope.message="Hello world";
+
+});
